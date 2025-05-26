5c87637ea2c36cbaa49d519cc17d9ee5d743e88e
Luppus-root <mrguerreroc@udistrital.edu.co>
2025-05-26 07:27:39 -0500

    Arreglo en estilos y NavBar

--- galeria-mascotas2.0/src/App.js
+++ galeria-mascotas2.0/src/App.js
@@ -1,23 +1,22 @@
+// App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Detail from "./components/Detail";
-
 import Mascotas from "./components/Mascotas";
-
 import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Mascotas />} />
           <Route path="/mascotas" element={<Mascotas />} />
           <Route path="/mascotas/:mascotaId" element={<Detail />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- galeria-mascotas2.0/src/components/Navbar.js
+++ galeria-mascotas2.0/src/components/Navbar.js
@@ -1,16 +1,21 @@
+// src/components/Navbar.js
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 function NavBar() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/mascotas">Adóptame</Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="dark" variant="dark">
+      <Container className="d-flex justify-content-between align-items-center">
+        <Button as={Link} to="/" variant="outline-light">
+          Inicio
+        </Button>
+        <Navbar.Brand as={Link} to="/mascotas">
+          Adóptame
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
